d181dd7e7a423cde7bf59e2af7531f50dfd646f0
Hrishikesh Kulkarni <hrishikeshparag@gmail.com>
2018-08-01 11:06:10 +0530

    Changes for refactoring.

---
+++ gcc/lto/lto-common.h
@@ -0,0 +1,41 @@
+/* LTO common.
+   Copyright (C) 2009-2018 Free Software Foundation, Inc.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+
+GCC is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+#ifndef LTO_COMMON_H
+#define LTO_COMMON_H
+
+void lto_init (void);
+void read_cgraph_and_symbols (unsigned, const char **);
+void offload_handle_link_vars (void);
+void do_whole_program_analysis (void);
+void materialize_cgraph (void);
+void print_lto_report_1 (void);
+
+extern int real_file_count;
+extern GTY((length ("real_file_count + 1"))) struct lto_file_decl_data **
+       real_file_decl_data;
+extern GTY((length ("lto_stats.num_input_files + 1"))) struct lto_file_decl_data **
+       all_file_decl_data;
+extern GTY(()) tree lto_eh_personality_decl;
+extern GTY(()) vec <tree, va_gc> *tree_with_vars;
+extern GTY(()) const unsigned char *lto_mode_identity_table;
+extern GTY(()) tree first_personality_decl;
+
+
+#endif
